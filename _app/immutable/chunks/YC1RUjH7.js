81694632097d4b36b5327a1883b2d263ee830b35
Br3nnabee <Br3nnabee@users.noreply.github.com>
2025-04-20 19:26:36 +0000

    deploy: fc88a07f91a2e1f24d6b5df82ea0a33827a45795

---
+++ _app/immutable/chunks/YC1RUjH7.js
@@ -0,0 +1,1 @@
+import{z as a}from"./B1hMIO0V.js";a();
---
+++ _app/immutable/nodes/3.BvsXI02Z.js
@@ -0,0 +1,1 @@
+import{t as o,a as t}from"../chunks/xaGwjEPO.js";import"../chunks/YC1RUjH7.js";var r=o("<p>Hi :3</p>");function n(p){var a=r();t(p,a)}export{n as component};
